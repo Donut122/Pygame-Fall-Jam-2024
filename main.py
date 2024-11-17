174893234efef670d622e60cf889659333fcb1cc
Donut <100806572+Donut122@users.noreply.github.com>
2024-11-17 23:23:12 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import pygame
-from engine import engine, create
 import sys
+import game
 
-width, height = 500, 500
-engine.start((width, height), vsync=False)
-engine.game.renderer.colour = (10, 10, 10)
+width, height = 64, 64
+window = pygame.display.set_mode((width, height), pygame.SCALED)
+clock = pygame.time.Clock()
 
-def on_start_button_click(): ...
+def on_start_button_click():
+    game.start()
 
-main_menu = create.UI.Frame(pygame.Rect(0, 0, width, height), (0, 0, 0))
-engine.game.UI.add(main_menu)
-
-title = create.UI.Text(pygame.Rect(width / 2, 0, 1, height / 8), "Game Title Here plz", center=True)
-start_button = create.UI.Button(
-    pygame.Rect(0, 0, width / 4, height / 8),
-    on_start_button_click,
-    colour=(0, 0, 0),
-    bg_colour=(25, 240, 25),
-    text="Start",
-    font="calibri",
-    align="center"
-)
-logo = create.UI.Image(pygame.Rect(0, 0, width / 4, height / 8), "title screen/pygame-ce logo", "bottomright")
-
-main_menu.add(title)
-main_menu.add(start_button)
-main_menu.add(logo)
-
-engine.lock_aspect_ratio()
+main_menu = pygame.image.load("assets/textures/main menu.png")
+start_button = pygame.Rect(0, 0, width, height)
+# button is the size of the screen for now
 
 while True:
-    for event in engine.get_events():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit(0)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                if start_button.collidepoint(event.pos):
+                    on_start_button_click()
         
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_ESCAPE: sys.exit(0)
 
-    engine.update()
+    window.blit(main_menu)
+
+    pygame.display.flip()
+    clock.tick(60)
